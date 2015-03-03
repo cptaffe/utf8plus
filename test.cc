d19a623b4641145da4273622a12645a4720f1975
Connor Taffe <cpaynetaffe@gmail.com>
2015-03-02 19:34:25 -0600

    upadate

--- test.cc
+++ test.cc
@@ -6,7 +6,7 @@
 // testing purposes
 int main() {
 	int32_t codepoint = 0x5555;
-	utf8::rune rune = utf8::rune::encode(codepoint).value;
+	utf8::rune rune = utf8::rune::encode(codepoint);
 	printf("U+%X->%#x->%.4s\n", codepoint, rune.decode(),  (char *) &rune.value);
 	printf("len: %d\n", (int) utf8::strlen((std::string *) "hello, world!"));
 	utf8::parser parser(std::string((char *) "hello, world!"));
